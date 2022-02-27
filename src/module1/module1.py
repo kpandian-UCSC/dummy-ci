37c1baf4343f524769cda81c98c5cf455b1447b2
Krishna Pandian <kpandian@ucsc.edu>
2022-02-26 18:21:06 -0800

    Fix to pylint error

--- src/module1/module1.py
+++ src/module1/module1.py
@@ -4,10 +4,10 @@ Implementation of Module 1
 
 import json
 
+# pylint: disable=W0613
 def lambda_handler(event, context):
     ''' Sample Lambda Function'''
     return {
         'statusCode': 200,
         'body': json.dumps('Hello To Yall!')
     }
-
