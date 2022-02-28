9db5e0797c38cfbdcb4222015b2f92728af47d6d
Krishna Pandian <kpandian@ucsc.edu>
2022-02-27 22:45:10 -0800

    Adding pip3 install boto for linter

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,8 +5,5 @@ aws_access_secret=$4
 local_path=$5
 
 
-# Install required dependencies for Python script.
-pip3 install boto3
-
 # Run upload script
 python3 scripts/pipeline/upload_file_to_s3.py $bucket_name $aws_key $aws_access_key $aws_access_secret $local_path
