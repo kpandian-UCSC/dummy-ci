c556254b78d5cc6278fffda1c3259b6784417b56
Krishna Pandian <kpandian@ucsc.edu>
2022-03-03 09:30:01 -0800

    test fix with dependencies

--- scripts/release.sh
+++
@@ -1,9 +0,0 @@
-bucket_name=$1
-aws_key=$2
-aws_access_key=$3
-aws_access_secret=$4
-local_path=$5
-
-
-# Run upload script
-python3 scripts/s3_upload.py $bucket_name $aws_key $aws_access_key $aws_access_secret $local_path
