57cf1c806e09426b38d7e0ee92d5d1a119f0d5bd
Krishna Pandian <kpandian@ucsc.edu>
2022-03-03 11:51:57 -0800

    test add env info functions

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -3,11 +3,13 @@ Implementation of Module 1
 '''
 
 import json
+import os
 
 # pylint: disable=W0613
 def lambda_handler(event, context):
     ''' Sample Lambda Function'''
+    env = os.environ.get('MY_ENV')
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello Lambda Deploy from same lambda packaging')
+        'body': json.dumps('Hello Lambda Deploy from same lambda' + env)
     }
