f6c558ea6c71e355b122d48b6e147b8442e0c8ae
Krishna Pandian <kpandian@ucsc.edu>
2022-03-04 17:50:19 -0800

    Test add module system and aws sm policies

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -4,12 +4,15 @@ Implementation of Module 1
 
 import json
 import os
+from secrets.manager import getLambdaSecret
 
 # pylint: disable=W0613
 def lambda_handler(event, context):
     ''' Sample Lambda Function'''
     env = os.environ.get('MY_ENV')
+    res = getLambdaSecret("MySecret")
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello Lambda Deploy from same lambda' + env)
+        'body': json.dumps('Hello Lambda Deploy from same lambda os: {env}, sm: {res}'.format(env=env, res=res))
     }
+print(lambda_handler(0,0))
