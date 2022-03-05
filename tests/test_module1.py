462c7f0ba7fe1cf54c1468a014a8e4b3e646ff46
Krishna Pandian <kpandian@ucsc.edu>
2022-03-04 18:27:20 -0800

    test imports fixed

--- tests/test_module1.py
+++ tests/test_module1.py
@@ -1,5 +1,11 @@
 ''' Module for Unit Testing '''
 
+from secrets.manager import get_lambda_secret
+
 def test_addition():
     ''' Testing an Addition '''
     assert 1+2 == 3
+
+def test_get_lambda_secret():
+    '''Testing Secret Function'''
+    assert get_lambda_secret("key") == "a key"
